a73ce8b4f0ff9e9a27cc981e3635c2c6ce188bec
rinne13 <varvararinne@gmail.com>
2024-08-16 10:37:05 +0300

    korjasin jsx-muodoksi

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
-  console.log(now, a+b)
-
-  return (
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-    </div>
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'Hello world, it is ', now.toString()
+    ),
+    React.createElement(
+      'p', null, a, ' plus ', b, ' is ', a + b
+    )
   )
 }
 
