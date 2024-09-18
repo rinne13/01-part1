6831944e36dcbb8ef6794e95f149b749bd6370a4
rinne13 <varvararinne@gmail.com>
2024-09-18 10:59:50 +0300

    tehty ennen React-sovellusten debuggas

--- src/App.jsx
+++ src/App.jsx
@@ -55,42 +55,30 @@ const App = () => {
 */
 
 const [left, setLeft] = useState(0)
-const [right, setRight] = useState(0)
-const [allClicks, setAll] = useState([])
-const [total, setTotal] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-const handleLeftClick = () => {
-  setAll(allClicks.concat('L'))
-  console.log('left before', left)
-  const updatedLeft = left + 1
-  setLeft(updatedLeft)
-  console.log('left after', left)
-  setTotal(updatedLeft + right)
-}
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-const handleRightClick = () => {
-  setAll(allClicks.concat('R'))
-  console.log('right before', right)
-  const updatedRight = right + 1
-  setRight(updatedRight)
-  console.log('right after', right)
-  setTotal(updatedRight + left)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
-}
-
-return (
-  <div>
+  return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <History allClicks={allClicks}/>
-      <p>total {total}</p>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
-  </div>
-)
-}
-  
+  )
+}  
 
 export default App
